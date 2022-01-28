93af08eb020362ce85399d123c786cdcb442ee39
vabarnabas <vabarnabas@gmail.com>
2022-01-28 21:52:39 +0100

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Navbar from "./Layout/Navbar"
+
+const App = () => {
+    return (
+        <div className="w-screen h-screen select-none">
+            <Navbar />
+        </div>
+    )
+}
+
+export default App
