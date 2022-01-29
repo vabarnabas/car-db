fe874c481c6f7bf593f02339e39a1e62cc070c47
vabarnabas <vabarnabas@gmail.com>
2022-01-29 19:43:13 +0100

    📄 End Commit

--- src/Components/Table/Table.tsx
+++ src/Components/Table/Table.tsx
@@ -6,14 +6,14 @@ interface Props {
 
 const Table: React.FC<Props> = ({ carList }) => {
     return (
-        <div className="w-full h-full flex items-center justify-center py-12 px-6 text-slate-700">
-            <div className="w-full md:flex md:items-center md:justify-center overflow-x-auto">
-            <table className='border border-slate-300'>
+        <div className="w-full h-full flex items-center justify-center py-16 px-6 text-slate-700">
+            <div className="w-full h-full md:flex md:items-center md:justify-center overflow-auto">
+            <table className=''>
                 <tbody>
                 {carList.map((row, index) => (
                     <tr key={index} className="border border-slate-300 first:font-bold">
                         {row.map((item, subIndex) => (
-                            <td key={subIndex} className="border border-slate-300 px-4 py-2 text-center capitalize">{index !== 0 && subIndex === 5 ? getCurrentDateTime(parseInt(item)) : item}</td>
+                            <td key={subIndex} className="border border-slate-300 px-4 py-2 text-center">{index !== 0 && subIndex === 5 ? getCurrentDateTime(parseInt(item)) : item}</td>
                         ))}
                     </tr>
                 ))}
